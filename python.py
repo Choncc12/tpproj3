ba27e330b948267aca318b708f7d1cda3a8685d4
RealK5 <53981116+RealK5@users.noreply.github.com>
2024-05-19 17:16:31 +0200

    Zamiana Pythona

--- python.py
+++ python.py
@@ -1,11 +1,21 @@
-import build.Debug.pybind11module as lib
+import visual.Debug.pybind11module as lib
 import numpy as np
 import os
 
-x = np.linspace(0, 10 * np.pi, 1000)
-y = np.sin(x)
 
-lib.plot_line(x,y)
+f = 10
+N = 1000
+t = np.linspace(0, 2 * np.pi, N)
+
+x = np.square(f * t)
+X= lib.DFT(x)
+Y = np.abs(X)
+#lib.plot_line(t,x)
+
+lib.plot_line(np.arange(N),x)
+lib.plot_line(np.arange(N),Y)
+lib.plot_line(np.arange(N),lib.IDFT(X))
+
 #say_hello(2)
 
 #square(100,10)
